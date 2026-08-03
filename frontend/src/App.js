2312aacb3fbef6addbe41fa1a7c41e45898a4623
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-03 16:39:00 -0400

    Convert routes to relative paths, and add an index path.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,11 +32,11 @@ const router = createBrowserRouter([
     path: "/",
     element: <RootLayout />,
     children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/events", element: <EventsPage /> },
-      { path: "/events/:eventId", element: <EventDetailPage /> },
-      { path: "/events/new", element: <NewEventPage /> },
-      { path: "/events/:eventId/edit", element: <EditEventPage /> },
+      { index: true, element: <HomePage /> },
+      { path: "events", element: <EventsPage /> },
+      { path: "events/:eventId", element: <EventDetailPage /> },
+      { path: "events/new", element: <NewEventPage /> },
+      { path: "events/:eventId/edit", element: <EditEventPage /> },
     ],
   },
 ]);
