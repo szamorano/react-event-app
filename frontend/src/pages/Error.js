75b579fe3406846978c33d773ef2d04e679b3eab
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-04 13:59:57 -0400

    Extract error data and throwing responses. Handled by generic error handling page.

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,5 +1,26 @@
+import { useRouteError } from "react-router-dom";
+import PageContent from "../components/PageContent";
+
 function ErrorPage() {
-  return <h1>An error occured!</h1>;
+  const error = useRouteError();
+
+  let title = "An error occurred";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    message = "Could not find resource or page.";
+    title = "Not found!";
+  }
+
+  return (
+    <PageContent title={title}>
+      <p>{message}</p>
+    </PageContent>
+  );
 }
 
 export default ErrorPage;
